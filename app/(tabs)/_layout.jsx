f63022e6e033237b929f96c608b730f9695b3ba5
molly <mnfelts@gmail.com>
2025-03-15 15:59:25 +0100

    tab setup & pages
    - index
    - profile
    - addnew

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+	return (
+		<Tabs
+		screenOptions={{
+			headerShown: false,
+		}}>
+			<Tabs.Screen name='index' />
+			<Tabs.Screen name='AddNew' />
+			<Tabs.Screen name='Profile' />
+		</Tabs>
+	)
+}
