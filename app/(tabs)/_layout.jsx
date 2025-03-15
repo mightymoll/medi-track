6f0df5b8b8961bb5ec106366014576ce06a47221
molly <mnfelts@gmail.com>
2025-03-15 16:11:48 +0100

    add icons to tabs bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,16 +1,35 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 export default function TabLayout() {
 	return (
 		<Tabs
 		screenOptions={{
-			headerShown: false,
+			headerShown: false,	
 		}}>
-			<Tabs.Screen name='index' />
-			<Tabs.Screen name='AddNew' />
-			<Tabs.Screen name='Profile' />
+			<Tabs.Screen name='index'
+			options={{
+				tabBarLabel: "Home",
+				tabBarIcon:({color, size})=>(
+				<FontAwesome name="home" size={size} color={color} />
+				)
+			}} />
+			<Tabs.Screen name='AddNew'
+						options={{
+				tabBarLabel: "Add New",
+				tabBarIcon:({color, size})=>(
+				<FontAwesome name="plus-square" size={size} color={color} />
+				)
+			}} />
+			<Tabs.Screen name='Profile'
+						options={{
+				tabBarLabel: "Profile",
+				tabBarIcon:({color, size})=>(
+				<FontAwesome name="user" size={size} color={color} />
+				)
+			}} />
 		</Tabs>
 	)
 }
