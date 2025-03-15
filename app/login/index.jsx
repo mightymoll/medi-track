2b11ce03f6a7d282bd4b63f080da63989c36feab
molly <mnfelts@gmail.com>
2025-03-15 17:24:27 +0100

    welcome page styling
    - layout & content
    - creation of colors.jsx to store global colors

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,5 +1,6 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import colors from '../../constant/colors';
 
 export default function LoginScreen() {
 	return (
@@ -12,14 +13,52 @@ export default function LoginScreen() {
 				<Image source={require('./../../assets/images/login.png')}
 				style={styles.image} />
 			</View>
+			<View style={{
+				padding: 25,
+				backgroundColor: colors.PRIMARY,
+        borderRadius: 10,
+				height: '100%',
+			}}>
+				<Text style={{
+					fontSize: 30,
+					fontWeight:'bold',
+					color: 'white',
+          textAlign: 'center',
+				}}>Stay on Track, Stay Healthy!</Text>
+				<Text style={{
+					fontSize: 17,
+					color: 'white',
+          textAlign: 'center',
+					marginTop: 20,
+					marginBottom: 20,
+				}}>Track your meds, take control of your health. Stay consistent, stay confident
+				</Text>
+				<TouchableOpacity style={styles.button}>
+					<Text style={{
+						textAlign:'center',
+						fontSize: 16,
+						color: colors.PRIMARY,
+					}}>Continue</Text>
+				</TouchableOpacity>
+				<Text style={{
+					color:'white',
+					marginTop: 10,
+				}}>Note: By clicking continue, you agree to our terms and conditions
+				</Text>
+			</View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	image:{
-		width: 200,
-    height: 450,
+		width: 130,
+    height: 300,
 		borderRadius: 23,
+	},
+	button:{
+		padding: 15,
+		backgroundColor: 'white',
+    borderRadius: 99,
 	}
 })
---
+++ constant/colors.jsx
@@ -0,0 +1,3 @@
+export default{
+	PRIMARY: '#bb27ff'
+}
